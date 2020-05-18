ac8b05d51c64b8e2650dd148e8eddf16f9c61cb7
Dhimas Prajaya <dhimasprajaya@gmail.com>
2020-05-18 22:31:49 +0700

    fetch order from firebase

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,8 +5,30 @@ import '../widgets/app_drawer.dart';
 import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = '/orders';
+
+  @override
+  _OrdersScreenState createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  var _isLoading = false;
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero).then((_) async {
+      setState(() {
+        _isLoading = true;
+      });
+      await Provider.of<Orders>(context, listen: false).fetchAndSetOrder();
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final orderData = Provider.of<Orders>(context);
@@ -16,9 +38,13 @@ class OrdersScreen extends StatelessWidget {
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-          itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
-          itemCount: orderData.orders.length),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+              itemCount: orderData.orders.length),
     );
   }
 }
