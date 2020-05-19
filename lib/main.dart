20d1ed6516fa15fa70c919f396428d545e5f484a
Dhimas Prajaya <dhimasprajaya@gmail.com>
2020-05-19 18:59:40 +0700

    add custom route all page transition

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
 import './providers/auth.dart';
+import './helpers/custom_route.dart';
 
 void main() {
   runApp(MyApp());
@@ -53,6 +54,12 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.purple,
             accentColor: Colors.deepOrange,
             fontFamily: 'Lato',
+            pageTransitionsTheme: PageTransitionsTheme(
+              builders: {
+                TargetPlatform.android: CustomPageTransitionBuilder(),
+                TargetPlatform.iOS: CustomPageTransitionBuilder(),
+              },
+            ),
             visualDensity: VisualDensity.adaptivePlatformDensity,
           ),
           home: auth.isAuth
