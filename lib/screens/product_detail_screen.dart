30e973bc853de6c493c75fe58540bf0c9349d650
Dhimas Prajaya <dhimasprajaya@gmail.com>
2020-05-19 18:14:12 +0700

    add place holder and hero image

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -28,7 +28,11 @@ class ProductDetailScreen extends StatelessWidget {
               Container(
                 height: 300,
                 width: double.infinity,
-                child: Image.network(loadedProduct.imageUrl, fit: BoxFit.cover),
+                child: Hero(
+                  tag: productId,
+                  child:
+                      Image.network(loadedProduct.imageUrl, fit: BoxFit.cover),
+                ),
               ),
               SizedBox(
                 height: 10,
